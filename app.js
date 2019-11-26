929e40618273b48215e86a9bb922d94095a1748c
Daniel Max <danielmaxsiegel@gmail.com>
2019-11-25 23:48:42 -0800

    tweet title

--- app.js
+++ app.js
@@ -46,16 +46,21 @@ http.get("http://api.qualitypoetry.com/create/poem?author=" + author + "&mood=" 
 
   client.post('statuses/update', {status: tweetContent})
     .then(function (tweet) {
-      console.log(tweet)
+      // console.log(tweet) // use for debugging
 
       // use id_str instead of id - #justJSONthings
       var newTweetId = tweet.id_str
 
+      var moodHashtag = '#' + mood
+      if (mood === 'basic') {
+        moodHashtag = ''
+      }
+
       // posting a reply requires mentioning the original tweet's poster
-      titleContent = 'I call this poem "' + title + '". For more, please follow @QualityPoetry.'
+      titleContent = 'I call this poem "' + title + '". For more, please follow @QualityPoetry #poetry ' + moodHashtag
       client.post('statuses/update', {status: titleContent, in_reply_to_status_id: newTweetId})
   	    .then(function (tweet) {
-          console.log(tweet)
+          // console.log(tweet) // use for debugging
   	    })
   	    .catch(function (error) {
   	    	throw error
