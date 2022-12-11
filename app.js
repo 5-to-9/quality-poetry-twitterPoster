cb3470b51c548d4c43e3de2adbe3e4fc067664f3
Dan Siegel <danielmaxsiegel@gmail.com>
2022-12-11 14:18:46 -0500

    use Poetry API V3

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var config = require('./config.js')
 
 var client = new Twitter(config)
 
-http.get("http://api.qualitypoetry.com/v2/create/poem", function(res) {
+http.get("http://api.qualitypoetry.com/v3/create/poem", function(res) {
   var rawResponse = ''
 
   res.on('data', function (chunk) {
@@ -12,40 +12,38 @@ http.get("http://api.qualitypoetry.com/v2/create/poem", function(res) {
   })
 
   res.on('end', function () {
-  var responseJSON = JSON.parse(rawResponse)
+    var response = JSON.parse(rawResponse)
+    var poemResponse = response.poem
 
-  var poem = responseJSON.poem
-  var title = poem.title
-  var lines = poem.lines
+    var gptStatusCode = response.gpt.status
 
-  var tweetContent = ''
-  var lineText = ''
-  var titleContent = ''
-  var lineStyle = ''
+    var poemTitle = poemResponse.title
 
-  for (var i = 0; i < lines.length; ++i) {
-    tweetContent += lines[i] + '\r\n'
-  }
+    var poem = ""
+    if (gptStatusCode == 200) {
+      poem = poemResponse.final
+    } else {
+      poem = poemResponse.raw
+    }
 
-  client.post('statuses/update', {status: tweetContent})
-    .then(function (tweet) {
-      // console.log(tweet) // use for debugging
+    client.post('statuses/update', {status: poem})
+      .then(function (tweet) {
+        // use id_str instead of id - #justJSONthings
+        var newTweetId = tweet.id_str
 
-      // use id_str instead of id - #justJSONthings
-      var newTweetId = tweet.id_str
+        // posting a reply requires mentioning the original tweet's poster
+        titleContent = 'I call this poem "' + poemTitle + '". For more, please follow @QualityPoetry #poems #poetry'
 
-      // posting a reply requires mentioning the original tweet's poster
-      titleContent = 'I call this poem "' + title + '". For more, please follow @QualityPoetry #poems #poetry'
-      client.post('statuses/update', {status: titleContent, in_reply_to_status_id: newTweetId})
-  	    .then(function (tweet) {
-          // console.log(tweet) // use for debugging
-  	    })
-  	    .catch(function (error) {
-  	    	throw error
-  	    })
-    })
-    .catch(function (error) {
-      throw error
-    })
+        client.post('statuses/update', {status: titleContent, in_reply_to_status_id: newTweetId})
+          .then(function (tweet) {
+            // console.log(tweet) // use for debugging
+          })
+          .catch(function (error) {
+            throw error
+          })
+      })
+      .catch(function (error) {
+        throw error
+      })
   })
 })
